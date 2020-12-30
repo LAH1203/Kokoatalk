2c9c33197036faa22ce3c8c2a298c3fe6677689c
LAH1203 <lah1203@naver.com>
2020-12-30 16:35:31 +0900

    Login ver1 & logo images

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # Kokoatalk
 Web page using node.js
+with LAH1203(Frontend - UI, UX, View), hyeji1221(Backend - Server, DB)
 
-with LAH1203, hyeji1221
+1. 로그인
+### 로그인 창 구현 ver1 - 이아현 (2020-12-30)
+##### login_page.html, login.js, style.css
+
+2. 회원가입
+
+3. 친구 목록
+
+4. 친구 검색 및 추가
+
+5. 채팅 창
---
+++ login.js
@@ -0,0 +1,74 @@
+function login() {
+    const form = document.login_form;
+    const user_email = checkValidEmail(form);
+    const user_password = checkValidPassword(form);
+
+    if (user_email) {
+        document.getElementById('alert_email').innerText = " ";
+        form.email.style.border = '2px solid';
+        form.email.style.borderColor = '#00D000';
+    } else {
+        form.email.style.border = '2px solid';
+        form.email.style.borderColor = '#FF0000';
+        document.getElementById('alert_email').style.color = '#FF0000';
+    }
+    if (user_password) {
+        document.getElementById('alert_password').innerText = " ";
+        form.email.style.border = '2px solid';
+        form.email.style.borderColor = '#00D000';
+    } else {
+        form.email.style.border = '2px solid';
+        form.email.style.borderColor = '#FF0000';
+        document.getElementById('alert_password').style.color = '#FF0000';
+    }
+
+    // 이메일과 패스워드가 모두 정확히 입력되었을 때
+    if (user_email && user_password) {
+        console.log('로그인 성공');
+    }
+}
+
+function checkValidEmail(form) {
+    // 아무것도 입력되지 않았을 경우
+    if (form.email.value == "") {
+        document.getElementById('alert_email').innerText = "이메일을 입력해주세요.";
+        return false;
+    }
+    // 이메일 형식(?@?.?)
+    const exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+    // 위에서 설정한 이메일 형식에 맞지 않을 때
+    if (exptext.test(form.email.value) == false) {
+        document.getElementById('alert_email').innerText = "이메일 형식에 맞지 않습니다.";
+        return false;
+    }
+    return true;
+}
+
+function checkValidPassword(form) {
+    // 아무것도 입력되지 않았을 경우
+    if (form.password.value == "") {
+        document.getElementById('alert_password').innerText = "패스워드를 입력해주세요.";
+        return false;
+    }
+    const pw = form.password.value;
+    // 알파벳
+    const eng = pw.search(/[a-z]/ig);
+    // 숫자
+    const num = pw.search(/[0-9]/g);
+    // 특수문자
+    const spch = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+    if (pw.length < 6) {
+        document.getElementById('alert_password').innerText = "6자 이상 입력해주세요.";
+        return false;
+    }
+    else if (pw.search(/\s/) != -1) {
+        document.getElementById('alert_password').innerText = "공백 없이 입력해주세요.";
+        return false;
+    }
+    else if (eng < 0 && num < 0 && spe < 0) {
+        document.getElementById('alert_password').innerText = "비밀번호를 제대로 입력해주세요.";
+        return false;
+    }
+    return true;
+}
