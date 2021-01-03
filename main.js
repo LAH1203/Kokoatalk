6dd1d94a56d2436e9487e9c38a8ed27af9a9e445
hyeji1221 <lhg2615@naver.com>
2021-01-03 21:05:33 +0900

    create server, 외장모듈 설치

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+var express = require('express'), http = require('http'), path = require('path');
+var bodyParser = require('body-parser'), static = require('serve-static'), errorHandler = require('express-error-handler');
+var fs = require('fs');
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var multer = require('multer');;
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+app.use('/public',static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(expressSession({
+    secret:'my key',
+    resave:true,
+    saveUninitialized:true
+}));
+
 http.createServer(app).listen(app.get('port'), function() {
     console.log('서버가 시작되었습니다');
 })
