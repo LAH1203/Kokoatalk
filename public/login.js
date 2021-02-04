da111cf00b683d71b070b0f695c2b793ad788afc
LAH1203 <lah1203@naver.com>
2021-02-04 14:24:08 +0900

    login, signup post 수정

--- public/login.js
+++ public/login.js
@@ -22,6 +22,7 @@ function login() {
     // DB 내에 해당 사람이 존재할 경우 로그인 성공, 아니면 실패로 해주세요!
     // 로그인이 성공했을 경우 /loginSuccess, 실패했을 경우 /loginFail로 이동하면 될 것 같습니당
     // 페이지 이동은 링크를 미리 만들어놨으므로 링크 이동 해주시면 됩니다.
+    /*
     router.get('/loginSuccess', function(req, res, next) {
       firebase.auth().signInWithEmailAndPassword(user_email, user_password)
       .then(function(firebaseUser){
@@ -31,7 +32,7 @@ function login() {
         res.redirect('error');
       })
     });
-
+    */
    
 
     // 이 부분에 사용자가 입력한 이메일과 패스워드가 DB 내에 존재하는지 여부 판단 코드를 써주시면 됩니다.
